83388c669b5e786d409fb9c0ad4059b8519471f5
Barxh <myxyq5@gmail.com>
2025-07-12 19:31:35 +0200

    finish IssueReportScreen

--- QuizApp/app/src/main/java/com/example/quizapp/presentation/issue_report/IssueReportScreen.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/presentation/issue_report/IssueReportScreen.kt
@@ -1,9 +1,27 @@
 package com.example.quizapp.presentation.issue_report
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.quizapp.presentation.issue_report.component.IssueReportScreenTopBar
 import com.example.quizapp.presentation.issue_report.component.QuestionCard
 
@@ -16,10 +34,111 @@ fun IssueReportScreen(modifier: Modifier = Modifier, state: IssueReportState) {
             title = "Report an Issue",
             onBackButtonClick = {}
         )
-        QuestionCard(
-            question = state.quizQuestion,
-            isCardExpanded = state.isQuestionCardExpanded,
-            onExpandClick = {}
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(10.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+
+            QuestionCard(
+                question = state.quizQuestion,
+                isCardExpanded = state.isQuestionCardExpanded,
+                onExpandClick = {}
+            )
+            Spacer(modifier.height(10.dp))
+            IssueTypeSection(
+                selectedIssueType = state.issueType,
+                otherIssueText = state.otherIssueText,
+                onOtherIssueTextChange = {},
+                onIssueTypeClick = {}
+            )
+            Spacer(modifier.height(10.dp))
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                value = state.additionalComment,
+                onValueChange = { },
+                label = {
+                    Text(text = "Additional Comment")
+                },
+                supportingText = { Text(text = "Describe the issue in more detail. (Optional)") }
+            )
+            Spacer(modifier.height(10.dp))
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = state.emailForFollowUp,
+                onValueChange = { },
+                label = {
+                    Text(text = "Email for Follow-up")
+                },
+                singleLine = true,
+                supportingText = { Text(text = "(Optional)") }
+
+            )
+
+        }
+        Button(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(10.dp),
+            onClick = {}
+        ) {
+            Text(
+                modifier = Modifier.padding(horizontal = 10.dp),
+                text = "Submit"
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+private fun IssueTypeSection(
+    modifier: Modifier = Modifier,
+    selectedIssueType: IssueType,
+    otherIssueText: String,
+    onOtherIssueTextChange: (String) -> Unit,
+    onIssueTypeClick: (IssueType) -> Unit
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = "ISSUE TYPE",
+            style = MaterialTheme.typography.bodySmall
         )
+        FlowRow {
+            IssueType.entries.forEach { issueType ->
+                Row(
+                    modifier = Modifier
+                        .widthIn(250.dp)
+                        .clickable { onIssueTypeClick(issueType) },
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = issueType == selectedIssueType,
+                        onClick = { onIssueTypeClick(issueType) }
+                    )
+                    if (issueType == IssueType.OTHER) {
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = otherIssueText,
+                            onValueChange = { onOtherIssueTextChange(it) },
+                            label = {
+                                Text(text = issueType.text)
+                            },
+                            singleLine = true,
+                            enabled = selectedIssueType == IssueType.OTHER
+                        )
+                    } else {
+                        Text(text = issueType.text)
+                    }
+
+                }
+            }
+        }
+
+
     }
 }
--- QuizApp/app/src/main/java/com/example/quizapp/presentation/issue_report/IssueReportState.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/presentation/issue_report/IssueReportState.kt
@@ -4,5 +4,16 @@ import com.example.quizapp.domain.model.QuizQuestion
 
 data class IssueReportState(
     val quizQuestion: QuizQuestion? = null,
-    val isQuestionCardExpanded: Boolean = false
+    val isQuestionCardExpanded: Boolean = false,
+    val issueType: IssueType = IssueType.OTHER,
+    val otherIssueText: String = "",
+    val additionalComment: String = "",
+    val emailForFollowUp: String = ""
 )
+
+enum class IssueType(val text: String){
+    INCORRECT_ANSWER(text = "Incorrect Answer"),
+    UNCLEAR_QUESTION(text = "Question is Unclear"),
+    TYPO_OR_GRAMMAR(text = "Typo or Grammar Mistake"),
+    OTHER(text = "Other")
+}
