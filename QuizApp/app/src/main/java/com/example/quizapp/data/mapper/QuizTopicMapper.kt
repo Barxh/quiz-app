e38c7850c6210dcfa948dcbaa304edb4096e8cb3
Barxh <myxyq5@gmail.com>
2025-08-24 23:31:15 +0200

    add room database

--- QuizApp/app/src/main/java/com/example/quizapp/data/mapper/QuizTopicMapper.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/data/mapper/QuizTopicMapper.kt
@@ -1,6 +1,7 @@
 package com.example.quizapp.data.mapper
 
 import com.example.quizapp.data.dto.QuizTopicDto
+import com.example.quizapp.data.local.entity.QuizTopicEntity
 import com.example.quizapp.data.util.Constant.BASE_URL
 import com.example.quizapp.domain.model.QuizTopic
 
@@ -10,4 +11,18 @@ fun QuizTopicDto.toQuizTopic() = QuizTopic(
     imageUrl = BASE_URL + imageUrl,
     code = code
 )
+fun QuizTopicDto.toQuizTopicEntity() = QuizTopicEntity(
+    id = id,
+    name = name,
+    imageUrl = BASE_URL + imageUrl,
+    code = code
+)
+fun QuizTopicEntity.toQuizTopic() = QuizTopic(
+    id = id,
+    name = name,
+    imageUrl = imageUrl,
+    code = code
+)
 fun List<QuizTopicDto>.toQuizTopics() = map { it.toQuizTopic() }
+fun List<QuizTopicDto>.toQuizTopicsEntities() = map { it.toQuizTopicEntity() }
+fun List<QuizTopicEntity>.toQuizTopics() = map { it.toQuizTopic() }
--- QuizApp/app/src/main/java/com/example/quizapp/data/util/Constant.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/data/util/Constant.kt
@@ -2,4 +2,6 @@ package com.example.quizapp.data.util
 
 object Constant {
     const val BASE_URL = "http://10.0.2.2:8080"
+    const val QUIZ_TOPIC_TABLE_NAME = "quiz_topics"
+    const val DATABASE_NAME = "quiz.db"
 }
